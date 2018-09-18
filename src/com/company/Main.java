824a8dfbdf4cbffd5688b812a733ee16bf9f4326
BT_1N3_03@BKTECH.LOCAL <BT_1N3_03@BKTECH.LOCAL>
2018-09-18 11:41:25 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+//run the method here
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("test method produces \t" + Pahmedlib.test("cat"));
+    }
+}
---
+++ src/com/company/Pahmedlib.java
@@ -0,0 +1,7 @@
+package com.company;
+//write method here
+public class Pahmedlib {
+    public static String test(String str) {
+        return str.substring(1);
+    }
+}
